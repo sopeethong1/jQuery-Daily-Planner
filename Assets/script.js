ca158037a888c61002c5ff42f428fa5eefc6acdc
Sopee Thong <tsopee@gmail.com>
2021-03-27 23:45:13 -0400

    removed bad code

--- Assets/script.js
+++ Assets/script.js
@@ -2,9 +2,6 @@ var hour = $("#hour");
 var event = $("#eventplaceholder");
 var save = $("#save");
 
-
-
-
 var time = moment().format('dddd, MMMM, Do');
 $("#currentDay").text(time);
 
@@ -12,9 +9,9 @@ var update = function() {
     document.getElementById("currentDay")
     .innerHTML = moment().format('dddd, MMMM, Do');
 }
-setInterval(update, 1000);
-
+setInterval(update, 1000);   
 
 $("#container-box").css({"background-color":"white", " border-bottom-color" :"black", "padding" : 0, "margin-bottom": "40px"});
 $("#hr").css ({"border-width": "10px", "border-color": "black"});
+
 
