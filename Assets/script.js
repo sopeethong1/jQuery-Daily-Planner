3ae7c25e7b0b21da03db0f68d189cb1e01e52c6e
Sopee Thong <tsopee@gmail.com>
2021-03-28 00:27:34 -0400

    adding time on row function

--- Assets/script.js
+++ Assets/script.js
@@ -14,5 +14,32 @@ setInterval(update, 1000);
 $("#container-box").css({"background-color":"white", " border-bottom-color" :"black", "padding" : 0, "margin-bottom": "40px"});
 $("#line").css ({"border-width": "10px", "border-color": "black"});
 
+function currentTime() {
+    var current = moment().hours()
+    var time = $("#row");
+
+    //    console.log(current);
+
+    time.each(function () {
+      var hour = parseInt($(this).attr('id'))
+
+    //  console.log(hour);
+
+      if (hour === current) {
+        $(this).children('.col-sm-8').attr('class', 'present col-sm-10 description')
+    //    console.log(('present' + hour + current))
+      } else if (current > hour) {
+        $(this).children('.col-sm-8').attr('class', 'past col-sm-10 description')
+    //    console.log(('past' + hour + current))
+      } else {
+        $(this).children('.col-sm-8').attr('class', 'future col-sm-10 description')
+    //    console.log(('future' + hour + current))
+      }
+    })
+  }
+  currentTime()
+
+
+
 
 
