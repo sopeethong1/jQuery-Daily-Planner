8cb0dd8e5914bb0ddf61d9260f6ae9fe901c7319
Sopee Thong <tsopee@gmail.com>
2021-03-27 20:57:39 -0400

    added css to container box div

--- Assets/script.js
+++ Assets/script.js
@@ -8,6 +8,9 @@ var update = function() {
 }
 setInterval(update, 1000);
 
+$("#container-box").css({"background-color":"white", " border-bottom-color" :"black", "padding" : 0, "margin-bottom": "40px"});
+$("#hr").css ({"border-width": "10px", "border-color": "black"});
 
 
+    
 
